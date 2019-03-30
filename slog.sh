7629e4b1fd2b1dde2207bcfbcd78023923ab3152
ZnHoCn <me@hakr.xyz>
2019-03-30 08:41:07 +0800

    Updated: 2019-03-30 08:41

--- slog.sh
+++ slog.sh
@@ -21,9 +21,9 @@ git push
 echo $(git push  2>&1 | grep fatal)
 
 if [ -z $(git push  2>&1 | grep fatal) ]; then
+    echo "git push ok"
+else
     echo "git push fatal"
     #rm -rf $(cd `dirname $0`; pwd)
-    git clone git@github.com:znhocn/test.git
-else
-    echo "git push ok"
+    #git clone git@github.com:znhocn/test.git
 fi
